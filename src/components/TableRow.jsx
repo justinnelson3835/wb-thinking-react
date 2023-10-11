ea1643055114c1680b35646e09e46b5f9f8101db
justinnelson3835 <justinnelson3835@gmail.com>
2023-10-11 16:39:36 -0600

    Second Commit

---
+++ src/components/TableRow.jsx
@@ -0,0 +1,20 @@
+import ModeButtons from "./ModeButtons"
+import DescriptionCell from "./DescriptionCell"
+import RateCell from "./RateCell"
+import HoursCell from "./HoursCell"
+import formatCurrency from "../utils/formatCurrency"
+
+const TableRow = ({ initialIsEditing, initialInvoiceData }) => {
+
+    return (
+        <tr>
+            <ModeButtons isEditing={initialIsEditing} />
+            <DescriptionCell isEditing={initialIsEditing} value={initialInvoiceData.description}/>
+            <RateCell isEditing={initialIsEditing} value={initialInvoiceData.rate}/>
+            <HoursCell isEditing={initialIsEditing} value={initialInvoiceData.hours}/>
+            <td>{formatCurrency(initialInvoiceData.rate * initialInvoiceData.hours)}</td>
+        </tr>
+    ) 
+}
+
+export default TableRow
